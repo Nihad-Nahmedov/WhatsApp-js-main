948c467b3c5bd998e3bca4339505133a1e0722fb
Nihad-NA <testaccount@gmail.com>
2023-08-29 01:31:22 +0400

    first commit

---
+++ app.js
@@ -0,0 +1,29 @@
+$(document).ready(function () {
+
+    $(document).on('keypress', function (e) {
+        if (e.which == 13) {
+            let url = `https://news-api-project-8401e868529d.herokuapp.com/api/news?api_key=4b1a0e44-0205-4ad8-9158-3c10c3e87ae9`
+            async function mynews() {
+                let responce = await fetch(url);
+                let data = await responce.json();
+                for (let i = 0; i < data.news.length; i++) {
+                    $("#city").append(`<h4>${data.news[i].tit}</h4>`);
+                }
+            }
+            mynews()
+        }
+    })
+
+    $("#btn").click(async function (e) {
+        e.preventDefault();
+        let url = `https://news-api-project-8401e868529d.herokuapp.com/api/news?api_key=4b1a0e44-0205-4ad8-9158-3c10c3e87ae9`
+        async function mynews() {
+            let responce = await fetch(url);
+            let data = await responce.json();
+            for (let i = 0; i < data.news.length; i++) {
+                $("#city").append(`<h4>${data.news[i].tit}</h4>`);
+            }
+        }
+        await mynews()
+    });
+})
